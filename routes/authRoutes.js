715ebea563c90783b099811a103c5b49035df0d4
VP Choudhary <vp.avvnl@gmail.com>
2026-05-06 01:17:14 +0530

    update backend

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,10 +2,9 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-// Registration Route
+// Routes
 router.post('/register', authController.register);
-
-// OTP Verification Route
 router.post('/verify-otp', authController.verifyOtp);
+router.post('/login', authController.login);
 
 module.exports = router;
